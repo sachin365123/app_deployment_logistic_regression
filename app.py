dc1883187399a66d9f092b21695a22ddaa871a47
Sachin Saxena <42056722+sachin365123@users.noreply.github.com>
2026-07-04 14:50:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 # -----------------------------------
 # Page Configuration
